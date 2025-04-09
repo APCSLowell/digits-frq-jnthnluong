720d68b4126f1f1f4334e9122cdc6c596fee00a3
jnthnluong <jnthnluong@gmail.com>
2025-04-09 12:25:04 -0700

    Update Digits.java

--- src/main/java/Digits.java
+++ src/main/java/Digits.java
@@ -19,7 +19,7 @@ public class Digits
 
 	public boolean isStrictlyIncreasing()
 	{ /* to be implemented in part (b) */
-		if(digitList.size() < 2) return true;
+		
 		for(int i = 0; i < digitList.size()-1; i++){
 			if(digitList.get(i) >= digitList.get(i+1))
 				return false;
